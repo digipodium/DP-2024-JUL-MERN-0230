62056e6f9e9367ed4692a782984f055f18060927
m-triple-m <triplem656@gmail.com>
2024-08-01 15:57:45 +0530

    ..

---
+++ Section 1/index.js
@@ -0,0 +1,11 @@
+console.log('Hello JavaScript');
+console.log('Have fun learning');
+
+num = 6543;
+
+console.log( num );
+console.log( typeof num );
+
+myname = "Mubassir";
+
+console.log( myname );
