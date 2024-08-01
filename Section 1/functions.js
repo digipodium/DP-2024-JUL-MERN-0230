f17a479563c1cf6bbc43874dd1010905bf73d6f4
m-triple-m <triplem656@gmail.com>
2024-08-01 15:58:22 +0530

    functions

---
+++ Section 1/functions.js
@@ -0,0 +1,17 @@
+function addNums(a, b){
+    var c = a+b;
+    // console.log(c);
+    return c;
+}
+
+const result = addNums(3, 4);
+// console.log(c);
+
+console.log(result);
+
+const getAvg = function(a, b, c){
+    return (a+b+c)/3;
+}
+
+const avg = getAvg(3, 4, 5);
+console.log(avg);
