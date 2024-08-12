feea3a839089a0e123ed95e53ff7715b747e8e06
m-triple-m <triplem656@gmail.com>
2024-08-12 15:54:01 +0530

    objects

--- Section 1/objects.js
+++ Section 1/objects.js
@@ -40,3 +40,70 @@ const perfectNums = nums.filter((n) => {
     return Number.isInteger(sqrt);
 });
 console.log(perfectNums);
+
+const smartphoneArray = [
+    {
+        brand: 'Samsung',
+        model: 'Galaxy M31',
+        price: 14999,
+        colors: ['black', 'white', 'blue']
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy S23',
+        price: 70000,
+        colors: ['black', 'oceanblue']
+    },
+    {
+        brand: 'Vivo',
+        model: 'S1 Pro',
+        price: 20000,
+        colors: ['blue', 'gray']
+    },
+    {
+        brand: 'Oppo',
+        model: 'Y11',
+        price: 24000,
+        colors: ['white', 'green']
+    },
+    {
+        brand: 'Apple',
+        model: '12 Mini',
+        price: 89999,
+        colors: ['black', 'white']
+    },
+    {
+        brand: 'Motorola',
+        model: 'G32',
+        price: 12999,
+        colors: ['green', 'blue']
+    },
+];
+
+console.log(smartphoneArray.length);
+console.log(smartphoneArray[3].price);
+
+// add a new color to motorola
+console.log(smartphoneArray[4].colors.push('red'));
+console.log(smartphoneArray);
+
+
+const budgetPhones = smartphoneArray.filter((phone) => { return phone.price <= 20000 });
+console.log(budgetPhones);
+
+const blackPhones = smartphoneArray.filter((phone) => { return phone.colors.includes('black') });
+console.log(blackPhones);
+
+const brands = smartphoneArray.map((phone) => { return phone.brand });
+console.log(brands);
+
+const keyword = 'sam';
+// const searchResult = smartphoneArray.filter((phone) => {
+//     return phone.brand.toLowerCase() === keyword.toLowerCase()
+// });
+
+const searchResult = smartphoneArray.filter((phone) => {
+    return phone.brand.toLowerCase().includes(keyword.toLowerCase())
+});
+
+console.log(searchResult);
