284fdfe0a426a891c2096ab7185e543f622dd82a
m-triple-m <triplem656@gmail.com>
2024-08-14 16:02:43 +0530

    components and routing

---
+++ Section 2/frontend/src/app/page.jsx
@@ -0,0 +1,22 @@
+import Input from '@/components/Input';
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>My Home Page</h1>
+      <p>ksgdh</p>
+      <br />
+      <input type="text" />
+
+      <button style={{ color: 'white', backgroundColor: 'black', padding: 10 }}>
+        Submit
+      </button>
+
+      <Input />
+
+    </div>
+  )
+}
+
+export default Home;
---
+++ Section 2/frontend/src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+    return (
+        <div>
+            <label htmlFor="">Name</label>
+            <input type="text" className='block px-3 py-1 w-full border' />
+        </div>
+    )
+}
+
+export default Input
