f24b60870e03e65e056a8b2a8e6f71659e3babdf
m-triple-m <triplem656@gmail.com>
2024-09-01 19:14:41 +0530

    read operation

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -8,11 +8,24 @@ router.post('/add', (req, res) => {
 
     new Model(req.body).save()
         .then((result) => {
-            res.json(result);
+            res.status(200).json(result);
         })
         .catch((err) => {
             console.log(err);
-            res.json(err);
+            res.status(500).json(err);
+        });
+
+})
+
+// getall
+router.get('/getall', (req, res) => {
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
         });
 
 })
