210f85b0e91e0ae4f9ce4db06f818cd85b423887
m-triple-m <triplem656@gmail.com>
2024-09-07 15:59:38 +0530

    manage user

--- Section 2/frontend/src/app/manageuser/page.jsx
+++ Section 2/frontend/src/app/manageuser/page.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+'use client';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const ManageUser = () => {
+
+    const [userList, setUserList] = useState([]);
+
+    const fetchUsers = async () => {
+        const res = await axios.get('http://localhost:5000/user/getall');
+        console.table(res.data);
+        setUserList(res.data);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const displayUsers = () => {
+        if (userList.length === 0) {
+            return <p>Loading...</p>
+        } else {
+            return <table className='w-full'>
+                <thead className='bg-blue-800 text-white text-left'>
+                    <tr>
+                        <th className='p-2'>ID</th>
+                        <th className='p-2'>Name</th>
+                        <th className='p-2'>Email</th>
+                        <th className='p-2'>City</th>
+                    </tr>
+                </thead>
+                <tbody className='bg-blue-200'>
+                    {
+                        userList.map((user) => {
+                            return <tr key={user._id}>
+                                <td className='p-2 border border-blue-400'>{user._id}</td>
+                                <td className='p-2 border border-blue-400'>{user.name}</td>
+                                <td className='p-2 border border-blue-400'>{user.email}</td>
+                                <td className='p-2 border border-blue-400'>{user.city}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        }
+    }
+
     return (
         <div>
             <div className='max-w-[80%] mx-auto border mt-8 p-6 shadow rounded-lg'>
                 <h1 className='text-center text-3xl font-bold'>ManageUsers</h1>
 
-                
+                {displayUsers()}
 
             </div>
         </div>
