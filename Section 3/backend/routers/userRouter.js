300c37dc96db2c5b78803f4ab7aaff4800e0a5bf
m-triple-m <triplem656@gmail.com>
2024-09-10 16:16:29 +0530

    jwt

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Model = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 router.post('/add', (req, res) => {
 
@@ -12,10 +14,10 @@ router.post('/add', (req, res) => {
         })
         .catch((err) => {
             console.log(err);
-            if(err.code === 11000){
-                res.status(500).json({ message : 'Email Already Exists' });
-            }else{
-                res.status(500).json({ message : 'Internal Server Error' });
+            if (err.code === 11000) {
+                res.status(500).json({ message: 'Email Already Exists' });
+            } else {
+                res.status(500).json({ message: 'Internal Server Error' });
             }
         });
 });
@@ -87,5 +89,38 @@ router.delete('/delete/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/authenticate', (req, res) => {
+    Model.findOne(req.body)
+        .then((result) => {
+
+            if (result) {
+                // generate token
+                const { _id, email, password } = result;
+                const payload = { _id, email, password };
+
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        if (err) {
+                            res.status(500).json({ message: 'Token Generation Failed' });
+                        } else {
+                            res.status(200).json({ token: token });
+                        }
+                    }
+                )
+
+
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 module.exports = router;
